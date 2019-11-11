c2fdbe2beaf4fc54d093e61fb2fd67d72ba7263d
katsuomi <>
2019-11-11 20:00:34 +0900

    requestテスト追加

--- spec/requests/maps_spec.rb
+++ spec/requests/maps_spec.rb
@@ -46,6 +46,33 @@ RSpec.describe MapsController, type: :request do
     end
   end
 
+  describe 'GET #edit' do
+    context 'マップが存在する場合' do
+      let(:house) { FactoryBot.create :house }
+
+      it 'リクエストが成功すること' do
+        get edit_map_url(house.id)
+        expect(response.status).to eq 200
+      end
+
+      it 'editテンプレートで表示されること' do
+        get edit_map_url(house.id)
+        expect(response).to render_template :edit
+      end
+
+      it '@mapが取得できていること' do
+        get edit_map_url(house.id)
+        expect(assigns :map).to eq house
+      end
+    end
+
+    context 'マップが存在しない場合' do
+      subject { -> { get edit_map_url(1) } }
+
+      it { is_expected.to raise_error ActiveRecord::RecordNotFound }
+    end
+  end
+
   describe 'POST #create' do
     context 'パラメータが妥当な場合' do
       it 'リクエストが成功すること' do
@@ -75,6 +102,41 @@ RSpec.describe MapsController, type: :request do
         expect do
           post maps_url, params: { map: FactoryBot.attributes_for(:map, :invalid) }
         end.to_not change(Map, :count)
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    let(:map) { FactoryBot.create :house }
+
+    context 'パラメータが妥当な場合' do
+      it 'リクエストが成功すること' do
+        put map_url map, params: { map: FactoryBot.attributes_for(:school) }
+        expect(response.status).to eq 302
+      end
+
+      it '住所が更新されること' do
+        expect do
+          put map_url map, params: { map: FactoryBot.attributes_for(:school) }
+        end.to change { Map.find(map.id).address }.to('名古屋大学')
+      end
+
+      it 'リダイレクトすること' do
+        put map_url map, params: { map: FactoryBot.attributes_for(:school) }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'パラメータが不正な場合' do
+      it 'リクエストが成功すること' do
+        put map_url map, params: { map: FactoryBot.attributes_for(:map, :invalid) }
+        expect(response.status).to eq 200
+      end
+
+      it '住所が変更されないこと' do
+        expect do
+          put map_url map, params: { map: FactoryBot.attributes_for(:map, :invalid) }
+        end.to_not change(Map.find(map.id), :address)
       end
     end
   end
